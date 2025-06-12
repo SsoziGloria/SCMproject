2fcc9bfe301aaf6dcf2c97fe74cf4f57284a0270
Arinda <arinda.emmanuelnsiimenta@students.mak.ac.ug>
2025-06-12 10:11:03 +0300

    Partial fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,28 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Chocolate SCM - @yield('title')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
+@include('layouts.head')
+
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="/">Chocolate SCM</a>
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('suppliers.index') }}">Suppliers</a>
-                    </li>
-                    <!-- Add more subsystem links later -->
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="container mt-4">
-        @yield('content')
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    @yield('content')
+    @include('layouts.scripts')
 </body>
+
 </html>
